aece674e0794500d4b0ff8efd3c5aaf2f9178523
Leegent <cavalar@163.com>
2016-04-29 20:40:37 +0800

    优化代码，分离事件与应用逻辑；修补确定按钮随时可点的bug

--- ife2016/task42/task42.js
+++ ife2016/task42/task42.js
@@ -29,8 +29,7 @@ function Datepicker(obj) {
     // 当前页面所在的年、月
     this.selectedYear = this.currentDate.getFullYear();
     this.selectedMonth = this.currentDate.getMonth() + 1;
-    // 多选模式下将currentDate置空
-    if (this.isMultiselect)this.currentDate = null;
+
     // 被选中日期，一定会被渲染。多选模式下可以将其置为-1
     this.selectedDate = -1;
     // 可选日期的范围
@@ -54,19 +53,23 @@ function Datepicker(obj) {
     );
     // 将DOM对象指回自身
     this.JQ[0].self = this;
-    // 日期部分单元格的数组
+    // 日期单元格的JQ对象集合
     this.dates = this.JQ.find("td");
     // 缓存年月下拉框
     this.yearSelector = this.JQ.find(".datepicker-select-year");
     this.monthSelector = this.JQ.find(".datepicker-select-month");
-    // 缓存确定、取消按钮
+    // 缓存确定按钮（因为要改变其可用性）
     this.okbtn = this.JQ.find(".datepicker-ok");
-    this.cancelbtn = this.JQ.find(".datepicker-cancel");
+
+    // 多选模式下的设置
+    if (this.isMultiselect){
+        this.currentDate = null;
+        this.okbtn.attr("disabled","disabled");
+    }
+
     // 给DOM设置指向该对象的指针
     this.yearSelector[0].datepicker = this;
     this.monthSelector[0].datepicker = this;
-    this.okbtn[0].datepicker = this;
-    this.cancelbtn[0].datepicker = this;
 
     // ================初始化界面=========================
     // 年下拉框添加option，以今年为准前推30年、后推5年
@@ -99,72 +102,62 @@ function Datepicker(obj) {
         this.datepicker.selectedMonth = $(this).val();
         this.datepicker.render();
     });
-    // 绑定事件：点击确定、取消按钮
-    this.okbtn.bind("click", function ok() {
-        // 向关联文本框输出日期
-        this.datepicker.outputDate();
-        // 清除已有选择
-        this.datepicker.reset();
-        this.datepicker.render();
-        // 改变日历可见性
-        this.datepicker.JQ.toggle();
-    });
-    this.cancelbtn.bind("click", function cancel() {
-        // 清除已有选择
-        this.datepicker.reset();
-        this.datepicker.render();
-        // 改变日历可见性
-        this.datepicker.JQ.toggle();
-    });
     // datepicker DIV充当点击事件代理
     this.JQ.bind("click", function (e) {
         var target = e.target || e.srcElement;
+        // 点击确定按钮
+        if(target.className == "datepicker-ok"){
+            // 向关联文本框输出日期
+            this.self.outputDate();
+            // 清除已有选择
+            this.self.reset();
+            this.self.render();
+            // 改变日历可见性
+            this.self.JQ.toggle();
+        }
+        // 点击取消按钮
+        else if(target.className == "datepicker-cancel"){
+            // 清除已有选择
+            this.self.reset();
+            this.self.render();
+            // 改变日历可见性
+            this.self.JQ.toggle();
+        }
         // 点上个月日期或者左箭头
         if (target.className == "datepicker-day-previousmonth" || target.className == "datepicker-arrow-left") {
-            // 1月的前一月是上一年的12月
-            if (this.self.selectedMonth === 1) {
-                this.self.selectedMonth = 12;
-                this.self.selectedYear--;
-            }
-            else this.self.selectedMonth--;
-            this.self.render();
+            this.self.switchToLastMonth();
         }
         // 点击下个月日期或右箭头
         else if (target.className == "datepicker-day-nextmonth" || target.className == "datepicker-arrow-right") {
-            // 12月的后一月是下一年的1月
-            if (this.self.selectedMonth === 12) {
-                this.self.selectedMonth = 1;
-                this.self.selectedYear++;
-            }
-            else this.self.selectedMonth++;
-            this.self.render();
+            this.self.switchToNextMonth();
         }
         // 点击当月可选日期
         else if (target.className.indexOf("datepicker-day-currentmonth") != -1) {
             // 多选模式交给专门的函数处理
             if (this.self.isMultiselect) {
-                this.self.clickMulti(target);
+                this.self.clickMulti(target.dataset.date);
             }
             else {
-                this.self.selectedDate = target.dataset.date;
-                this.self.render();
-                // 若设置了回调函数则执行之
-                if (this.self.callback) this.self.callback();
+                this.self.clickSingle(target.dataset.date);
             }
         }
     });
 }
+// =====set函数====
 // 恢复默认值
 Datepicker.prototype.reset = function () {
     this.currentDate = new Date();
     this.secondDate = null;
     this.selectedYear = this.currentDate.getFullYear();
     this.selectedMonth = this.currentDate.getMonth() + 1;
-    if (this.isMultiselect) this.currentDate = null;
-    this.selectedDate = -1;
-    this.state = 0;
+    if (this.isMultiselect) {
+        this.currentDate = null;
+        this.selectedDate = -1;
+        this.state = 0;
+        this.okbtn.attr("disabled","disabled");
+    }
 };
-// 设置选中的日期对象。只有在点击可选日期时才会调用该函数
+// 设置选择的日期对象。只有在点击可选日期时才会调用该函数
 Datepicker.prototype.setCurrentDate = function () {
     if (this.currentDate != null) {
         this.currentDate.setFullYear(this.selectedYear);
@@ -175,10 +168,6 @@ Datepicker.prototype.setCurrentDate = function () {
         this.currentDate = new Date((this.selectedMonth) + "/" + this.selectedDate + "/" + this.selectedYear);
     }
 };
-// 返回选中的日期对象
-Datepicker.prototype.getCurrentDate = function () {
-    return this.currentDate;
-};
 /**
  * 设置选中日期的接口
  * @param {Date} obj 要设置的日期
@@ -187,11 +176,21 @@ Datepicker.prototype.setSelected = function (obj) {
     this.selectedYear = obj.getFullYear();
     this.selectedMonth = obj.getMonth() + 1;
     this.selectedDate = obj.getDate();
-    this.outputDate();
+    this.setCurrentDate();
     this.render();
 };
+// 设置选择日期后的回调函数
+Datepicker.prototype.setCallback = function (cb) {
+    this.callback = cb;
+};
+
+// ======get函数====
+// 返回选中的日期对象
+Datepicker.prototype.getCurrentDate = function () {
+    return this.currentDate;
+};
 // 获取选中日期的"YYYY-mm-DD"格式字符串
-Datepicker.prototype.formatDate = function (date) {
+Datepicker.prototype.getFormatDate = function (date) {
     if (date instanceof Date)    return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
     else return "";
 };
@@ -201,26 +200,14 @@ Datepicker.prototype.formatDate = function (date) {
  * @param {Date} date2
  * @returns {Number}
  */
-Datepicker.prototype.dateDiff = function (date1, date2) {
+Datepicker.prototype.getDateDiff = function (date1, date2) {
     if (!(date1 instanceof Date) || !(date2 instanceof Date)) return null;
     date1.setHours(0, 0, 0, 0);
     date2.setHours(0, 0, 0, 0);
-    return (date2 - date1) / (1000 * 60 * 60 * 24);
+    return ((date2 - date1) / (1000 * 60 * 60 * 24));
 };
-// 向关联的DOM元素输出日期
-Datepicker.prototype.outputDate = function () {
-    if (!this.isMultiselect) {
-        this.targetJQ.val(this.formatDate(this.currentDate));
-    }
-    else {
-        if (this.currentDate < this.secondDate) {
-            this.targetJQ.val(this.formatDate(this.currentDate) + " 至 " + this.formatDate(this.secondDate));
-        }
-        else {
-            this.targetJQ.val(this.formatDate(this.secondDate) + " 至 " + this.formatDate(this.currentDate));
-        }
-    }
-};
+
+// ======判断函数=====
 /**
  * 判断一个日期是否在可选范围内
  * @param {Date} day 日期
@@ -228,57 +215,89 @@ Datepicker.prototype.outputDate = function () {
 Datepicker.prototype.isSelectable = function (day) {
     return !!(day >= this.startDay && day <= this.endDay);
 };
-// 设置选择日期后的回调函数
-Datepicker.prototype.setCallback = function (cb) {
-    this.callback = cb;
-};
-// 将多选模式的点击事件分离出来写成函数
-// 判断是否处在选择的时间段
-Datepicker.prototype.isInTimeSegment = function (day) {
+/**
+ * 判断是否处在选择的时间段
+ * @param {Date} date 要判断的日期
+ * @returns {boolean}
+ */
+Datepicker.prototype.isInTimeSegment = function (date) {
     // 只在模式2下有可能返回true
     if (this.state <= 1) return false;
-    return !!((day >= this.currentDate && day <= this.secondDate) || (day >= this.secondDate && day <= this.currentDate))
+    return !!((date >= this.currentDate && date <= this.secondDate) || (date >= this.secondDate && date <= this.currentDate))
 };
-// 多选模式下的点击代理，target是一个可选范围内的日期DOM。该函数作用是切换状态
-Datepicker.prototype.clickMulti = function (target) {
+
+// =====应用逻辑函数=====
+// 切换到上个月的显示界面
+Datepicker.prototype.switchToLastMonth = function () {
+    // 1月的前一月是上一年的12月
+    if (this.selectedMonth === 1) {
+        this.selectedMonth = 12;
+        this.selectedYear--;
+    }
+    else this.selectedMonth--;
+    this.render();
+};
+// 切换到下个月的显示界面
+Datepicker.prototype.switchToNextMonth = function () {
+    // 12月的后一月是下一年的1月
+    if (this.selectedMonth === 12) {
+        this.selectedMonth = 1;
+        this.selectedYear++;
+    }
+    else this.selectedMonth++;
+    this.render();
+}
+// 单选模式下点击可选日期后的动作
+Datepicker.prototype.clickSingle = function (targetdate) {
+    this.selectedDate = targetdate;
+    this.render();
+    // 若设置了回调函数则执行之
+    if (this.callback) this.callback();
+}
+// 多选模式下点击可选日期后的动作。该函数作用是切换状态
+Datepicker.prototype.clickMulti = function (targetdate) {
     // 状态0，未选择任何日期
     if (this.state == 0) {
-        this.selectedDate = target.dataset.date;
+        this.selectedDate = targetdate;
         this.state = 1;
         this.setCurrentDate();
+        this.okbtn.attr("disabled","disabled");
         this.render();
     }
     // 状态1，已选择了第一个日期
     else if (this.state == 1) {
         // 当前点击的年月日的临时变量
-        var tmpdate = new Date(this.selectedMonth + "/" + target.dataset.date + "/" + this.selectedYear);
-        var diff = Math.abs(this.dateDiff(tmpdate, this.currentDate));
+        var tmpdate = new Date(this.selectedMonth + "/" + targetdate + "/" + this.selectedYear);
+        // 时间跨度等于首末日之差再加1
+        var timespan = Math.abs(this.getDateDiff(tmpdate, this.currentDate))+1;
         // 若重复点击已选日期，则什么也不做
-        if (diff == 0) return;
+        if (timespan == 1) return;
         // 判断一下是否满足时间跨度要求，不满足时阻止此次选择并给出相应提示
-        if (this.maxLen > 0 && diff > this.maxLen) {
-            alert("您选择的日期跨度为" + diff + "天，超过了" + this.maxLen + "天的最大跨度！");
+        if (this.maxLen > 0 && timespan > this.maxLen) {
+            alert("您选择的日期跨度为" + timespan + "天，超过了" + this.maxLen + "天的最大跨度！");
             return;
         }
-        if (this.minLen > 0 && diff < this.minLen) {
-            alert("您选择的日期跨度为" + diff + "天，低于了" + this.minLen + "天的最小跨度！");
+        if (this.minLen > 0 && timespan < this.minLen) {
+            alert("您选择的日期跨度为" + timespan + "天，低于了" + this.minLen + "天的最小跨度！");
             return;
         }
         // 将第二个日期指向该变量
         this.secondDate = tmpdate;
         this.state = 2;
+        this.okbtn.removeAttr("disabled");
         this.render();
     }
     // 状态3，已经选中了一个时间段 ，再点则将刚点击的点作为新的currentDate，回到状态1
     else {
-        this.selectedDate = target.dataset.date;
+        this.selectedDate = targetdate;
         this.setCurrentDate();
         this.secondDate = null;
         this.state = 1;
+        this.okbtn.attr("disabled","disabled");
         this.render();
     }
 };
-// 渲染年、月和日期选择页面
+// 渲染
 Datepicker.prototype.render = function () {
     // 渲染年月
     this.yearSelector.val(this.selectedYear);
@@ -310,7 +329,7 @@ Datepicker.prototype.render = function () {
         if (this.isSelectable(tmp)) {
             // 用data-date自定义属性存储日期，可以用dataset来取
             $(this.dates[i]).removeClass().addClass("datepicker-day-currentmonth").attr("data-date", k).html(k);
-            if (this.dateDiff(tmp, this.currentDate) == 0 || this.dateDiff(tmp, this.secondDate) == 0) {
+            if (this.getDateDiff(tmp, this.currentDate) == 0 || this.getDateDiff(tmp, this.secondDate) == 0) {
                 $(this.dates[i]).addClass("datepicker-day-selected");
             }
             if (this.isInTimeSegment(tmp)) $(this.dates[i]).addClass("datepicker-day-timeSegment");
@@ -325,6 +344,21 @@ Datepicker.prototype.render = function () {
         $(this.dates[i]).removeClass().addClass('datepicker-day-nextmonth').html(k);
     }
 };
+// 向关联的DOM元素输出日期
+Datepicker.prototype.outputDate = function () {
+    if (!this.isMultiselect) {
+        this.targetJQ.val(this.getFormatDate(this.currentDate));
+    }
+    else {
+        if (this.currentDate < this.secondDate) {
+            this.targetJQ.val(this.getFormatDate(this.currentDate) + " 至 " + this.getFormatDate(this.secondDate));
+        }
+        else {
+            this.targetJQ.val(this.getFormatDate(this.secondDate) + " 至 " + this.getFormatDate(this.currentDate));
+        }
+    }
+};
+
 //=================================demo===============================================
 var start = new Date("4/6/2016");
 var end = new Date("6/22/2016");
